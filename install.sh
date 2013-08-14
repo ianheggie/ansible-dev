4d4ccb2faec727fb501ead9b4cce7bc7eaad9cd0
Jon Doveston <jon.doveston@onthebeach.co.uk>
2013-08-14 11:32:49 +0100

    elasticsearch, memcached, fix nodejs, add to rails etc

--- install.sh
+++ install.sh
@@ -4,6 +4,8 @@ if [ -f /home/vagrant/.bashrc ] && [ ! -L /home/vagrant/.bashrc ]
 then
   rm -fv /home/vagrant/.bashrc
 fi
-cp -fv /home/vagrant/ssh/id_rsa* /home/vagrant/.ssh
+cp -fv /home/vagrant/ssh/id_rsa /home/vagrant/.ssh
+cp -fv /home/vagrant/ssh/id_rsa.pub /home/vagrant/.ssh
+cp -fv /home/vagrant/ssh/vagrant /home/vagrant/.ssh
 cp -fv /home/vagrant/ssh/config /home/vagrant/.ssh
 chown -Rv vagrant:vagrant /home/vagrant/.ssh
