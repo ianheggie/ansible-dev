56308272bf2a0070a2a35d6947693779ee3ed84a
Jon Doveston <jon.doveston@onthebeach.co.uk>
2013-08-20 16:09:19 +0100

    change install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,9 @@ then
   rm -fv /home/vagrant/.bashrc
 fi
 cp -fv /home/vagrant/ssh/id_rsa /home/vagrant/.ssh
+chmod -v 600 /home/vagrant/.ssh/id_rsa
 cp -fv /home/vagrant/ssh/id_rsa.pub /home/vagrant/.ssh
 cp -fv /home/vagrant/ssh/vagrant /home/vagrant/.ssh
-cp -fv /home/vagrant/ssh/config /home/vagrant/.ssh
+chmod -v 600 /home/vagrant/.ssh/vagrant
+cp -uv /home/vagrant/ssh/config /home/vagrant/.ssh
 chown -Rv vagrant:vagrant /home/vagrant/.ssh
